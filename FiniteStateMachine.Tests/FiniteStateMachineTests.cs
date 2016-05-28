7c9d71eef8cc3b139ac65908e4e855b11d0d399e
Skyblade <qlzp2@mail.ru>
2016-05-28 18:19:40 +0300

    Add source code

---
+++ FiniteStateMachine.Tests/FiniteStateMachineTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Skyblade;
+
+namespace FiniteStateMachine.Tests
+{
+    [TestClass]
+    public class FiniteStateMachineTests
+    {
+        [TestMethod]
+        public void InitialState()
+        {
+            Prototype prototype = new Prototype();
+            Alpha alpha = new Alpha();
+            Release release = new Release();
+
+            FsmSystem<State> fsm = new FsmSystem<State>();
+            fsm.AddTransition(prototype, Transitions.Fix, alpha);
+            fsm.AddTransition(alpha, Transitions.Optimize, release);
+
+            fsm.SetInitialState(prototype);
+
+            fsm.PerformTransition(Transitions.Fix);
+            fsm.PerformTransition(Transitions.Optimize);
+
+            Assert.AreEqual(fsm.GetCurrentState(), release);
+        }
+
+        private abstract class State
+        {
+            protected abstract string MakeIt();
+        }
+
+        private class Prototype : State
+        {
+            protected override string MakeIt()
+            {
+                return "Make it";
+            }
+        }
+
+        private class Alpha : State
+        {
+            protected override string MakeIt()
+            {
+                return "Make it right";
+            }
+        }
+
+        private class Release : State
+        {
+            protected override string MakeIt()
+            {
+                return "Make it fast";
+            }
+        }
+
+        private enum Transitions
+        {
+            Fix,
+            Optimize
+        }
+    }
+}
---
+++ FiniteStateMachine/FsmSystem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Skyblade
+{
+    public class FsmSystem<TState> where TState: class
+    {
+        private Dictionary<TState, Dictionary<object, TState>> routes = new Dictionary<TState, Dictionary<object, TState>>();
+        private TState currentState;
+
+        public TState GetCurrentState()
+        {
+            return currentState;
+        }
+
+        public void AddTransition<TEnum>(TState from, TEnum transition, TState to) where TEnum : struct, IConvertible, IComparable, IFormattable
+        {
+            CheckEnum<TEnum>();
+
+            if (!routes.ContainsKey(from))
+            {
+                routes.Add(from, new Dictionary<object, TState>());
+            }
+
+            routes[from][transition] = to;
+        }
+
+        public void PerformTransition<TEnum>(TEnum transition) where TEnum : struct, IConvertible, IComparable, IFormattable
+        {
+            CheckEnum<TEnum>();
+
+            TState nextState = routes[currentState][transition];
+            currentState = nextState;
+        }
+
+        public void SetInitialState(TState initialState)
+        {
+            if (!routes.ContainsKey(initialState))
+            {
+                throw new ArgumentException("State machine does not contain this state.", "initialState");
+            }
+
+            currentState = initialState;
+        }
+
+        private void CheckEnum<TEnum>() where TEnum : struct, IConvertible, IComparable, IFormattable
+        {
+            if (!typeof(TEnum).IsEnum)
+            {
+                throw new ArgumentException("TEnum must be an enum.");
+            }
+        }
+    }
+}
